44bd07e0dba9dc04a1ec4e510853031c9535b7be
Leonardo Lorenzini <lorenzini.leo@gmail.com>
2020-04-26 18:32:29 +0100

    Started FIBD problem

---
+++ FIBD/fibd.py
@@ -0,0 +1,6 @@
+"""Given: Positive integers n≤100 and m≤20
+Return: The total number of pairs of rabbits that will remain after the n-th month if all rabbits live for m months."""
+
+with open ('rosalind_fibd.py', 'r') as input_file:
+    parameters = input_file.read().split()
+    
